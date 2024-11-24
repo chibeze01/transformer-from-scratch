68b636fb19546e61a0450758643f3629ca4c3de0
Chib master <chibeze2@hotmail.co.uk>
2024-11-24 23:04:36 +0000

    Initial commit

---
+++ bigramLM.py
@@ -0,0 +1,158 @@
+from train import get_batch, device, eval_iters, n_embd, block_size, n_heads, dropout, n_layers, max_iters, learning_rate
+from prepare import decode, encode, vocab_size
+import torch
+import matplotlib.pyplot as plt
+
+class Head(torch.nn.Module):
+    ''' single attention head'''
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.key = torch.nn.Linear(n_embd, n_embd)
+        self.query = torch.nn.Linear(n_embd, n_embd)
+        self.value = torch.nn.Linear(n_embd, n_embd)
+        self.register_buffer('mask', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, x):
+        B, T, C = x.shape
+        # get the key, query, and value
+        k = self.key(x) # (B T C)
+        q = self.query(x) # (B T C)
+        v = self.value(x) # (B T C)
+        # compute the dot product attention
+        weights = q @ k.transpose(-1, -2) * C**-0.5 # (B T T)
+        weights = weights.masked_fill(self.mask[:T, :T] == 0, float('-inf'))
+        weights = torch.nn.functional.softmax(weights, dim=-1)
+        weights = self.dropout(weights)
+        y = weights @ v # (B T C)
+        return y
+    
+class multiHeadAttention(torch.nn.Module):
+    def __init__(self, n_heads, head_size):
+        super().__init__()
+        self.heads = torch.nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.proj = torch.nn.Linear(n_embd, n_embd)
+        self.dropout = torch.nn.Dropout(dropout)
+    
+    def forward(self, x):
+        B, T, C = x.shape
+        # apply all the heads
+        y = torch.cat([h(x) for h in self.heads], dim=-1)
+        y = self.dropout(self.proj(y))
+        return y
+    
+class FFN(torch.nn.Module):
+# RELU non linearity
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Linear(n_embd, 4 * n_embd),
+            torch.nn.ReLU(),
+            torch.nn.Linear(4 * n_embd, n_embd),
+            torch.nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class Block(torch.nn.Module):
+    def __init__(self, n_embd, n_heads):
+        super().__init__()
+        self.multiHeadAttention = multiHeadAttention(n_heads, n_embd//n_heads)
+        self.FFN = FFN(n_embd)
+        self.norm1 = torch.nn.LayerNorm(n_embd)
+        self.norm2 = torch.nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.multiHeadAttention(self.norm1(x))
+        x = x + self.FFN(self.norm2(x))
+        return x
+    
+class BigramLM(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(vocab_size, n_embd) # b t c
+        self.sa_head = Head(n_embd)
+        self.multiHeadAttention = multiHeadAttention(n_heads, n_embd//n_heads)
+        self.FFN = FFN(n_embd)
+        self.lm_head = torch.nn.Linear(n_embd, vocab_size)
+        self.blocks = torch.nn.Sequential(
+            *[Block(n_embd, n_heads) for _ in range(n_layers)],
+            torch.nn.LayerNorm(n_embd)
+        )
+        # position embedding table
+        self.positional_embeddings = torch.nn.Embedding(block_size, n_embd)
+
+    def forward(self, idx, target =None):
+        B, T = idx.shape
+
+        # get the positional embeddings
+        pos_embd = self.positional_embeddings(torch.arange(T, device=device)) # (T C)
+        tok_embd = self.embedding(idx) # (B T C)
+        x = tok_embd + pos_embd # (B T C)
+        x = self.blocks(x) # (B T C)
+        logits = self.lm_head(x) # (B T V)
+        # v != c && v = vocab_size
+
+        if target is not None:
+            loss = torch.nn.CrossEntropyLoss()(logits.view(-1, vocab_size), target.view(-1))
+        else:
+            loss = None
+            
+        return logits, loss
+    
+    def generate(self, idx, max):
+        for _ in range(max):
+            idx_croped = idx[:, -block_size:] # (B T)
+            logits, _ = self(idx_croped) # get the logits (B T C)
+            # drop the time sequence dimension
+            logits = logits[:, -1, :] # (B C)
+            probs = torch.nn.functional.softmax(logits, dim=-1)
+            # sample from the distribution
+            next_idx = torch.multinomial(probs, num_samples=1) # (B 1)
+            # append to the sequence
+            idx = torch.cat([idx, next_idx], dim=1) # (B T+1)
+        return idx
+    
+model = BigramLM()
+model = model.to(device)
+
+@torch.no_grad()
+def evaluate_Loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for i in range(eval_iters):
+            x, y = get_batch(split)
+            _ , loss = model(x, y)
+            losses[i] = loss
+        out[split] = losses.mean().item()
+    model.train()
+    return out
+
+# train the model
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for i in range(max_iters):
+
+    if i % 100 == 0:
+        losses = evaluate_Loss()
+        print(f"train loss: {losses['train']:.4f} val loss: {losses['val']:.4f}")
+
+    # sample a batch of data
+    x, y = get_batch('train')
+
+    # forward pass
+    logits, loss = model(x, y)
+    # backward pass
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+
+
+
+
+# print(decode(model.generate(torch.zeros((1,1), dtype=torch.long, device=device), 10000)[0].tolist())) # generate some 10,000 char shakespeare
---
+++ train.py
@@ -0,0 +1,40 @@
+import os
+import torch
+import numpy as np
+
+
+device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device(device_type)
+data_dir = os.path.join(os.path.dirname(__file__))
+batch_size = 64
+block_size = 256
+max_iters = 5000
+eval_iters = 500
+n_embd = 384
+n_heads = 6
+dropout = 0.2
+n_layers = 6
+learning_rate = 3e-4
+torch.manual_seed(1337)
+
+def get_batch(split):
+    # We recreate np.memmap every batch to avoid a memory leak, as per
+    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+    if split == 'train':
+        data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
+    else:
+        data = np.memmap(os.path.join(data_dir, 'val.bin'), dtype=np.uint16, mode='r')
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
+    if device_type == 'cuda':
+        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+    else:
+        x, y = x.to(device), y.to(device)
+    return x, y
+
+
+if __name__ == "__main__":
+    X , Y = get_batch('train')
+    print (X.shape, Y.shape, "-- Shape of X and Y"), print (X[0], Y[0], "-- First batch of X and Y")
